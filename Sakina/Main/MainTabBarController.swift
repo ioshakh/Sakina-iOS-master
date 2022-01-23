b8c3b20320e1429584d5893db17bfc90f71932be
Shakhzod <79221577+ioshakh@users.noreply.github.com>
2022-01-23 17:37:00 +0500

    Initial Commit

---
+++ Sakina/Main/MainTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  MainBottomTabbarController.swift
+//  Layusraf
+//
+//  Created by Mirzabek on 23/01/22.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+    }
+}
